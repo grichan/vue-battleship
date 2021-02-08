546658183c49f266c6f686b92eabe50a7fb057e2
Vlad <vladgrichan@gmail.com>
2021-02-08 12:12:13 +0000

    added the fields

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,107 @@
 <template>
-  <div class="main"></div>
+  <header>Battleship Game 🚢</header>
+  <div class="main">
+    <!-- {{ playerField }} -->
+    <div class="player field">
+      <div class="row" v-for="(row, y) in playerField" :key="y">
+        <div
+          @click="placeShip($event)"
+          class="cell"
+          v-for="(col, x) in row"
+          :key="col"
+          :data-position="y + '-' + x"
+        >
+          {{ col }}
+        </div>
+      </div>
+    </div>
+    <div>
+      <button>Start new game</button>
+      <p>Your turn</p>
+      <div>Ships to place remaining:</div>
+      <div v-for="ship in playerShips" :key="ship">
+        {{ ship[0] }}x{{ ship[1] }}
+        <span v-for="i in ship[0]" :key="i">
+          🚢
+        </span>
+      </div>
+    </div>
+    <div class="enemy field">
+      <div class="row" v-for="(row, key) in enemyField" :key="key">
+        <div class="cell" v-for="col in row" :key="col">
+          {{ col }}
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {}
+  components: {},
+  methods: {
+    placeShip(e) {
+      e.preventDefault();
+      console.log("e :>> ", e.target.dataset);
+      console.log("object :>> ");
+    }
+  },
+  data() {
+    return {
+      playerField: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ],
+      playerShips: [
+        [2, 4],
+        [1, 5]
+      ],
+      enemyField: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ]
+    };
+  }
 };
 </script>
 
-<style></style>
+<style>
+.main {
+  display: flex;
+  justify-content: space-evenly;
+}
+.field {
+  margin: 10px;
+}
+.row {
+  display: flex;
+}
+
+.cell {
+  margin: 1px;
+  height: 30px;
+  width: 30px;
+  background-color: rgb(240, 255, 253);
+}
+
+.enemy .cell {
+  background-color: seashell;
+}
+</style>
--- tests/unit/App.spec.js
+++ tests/unit/App.spec.js
@@ -4,17 +4,55 @@ import App from "@/App.vue";
 let wrapper = null;
 
 beforeEach(() => {
-  wrapper = shallowMount(App);
+  wrapper = shallowMount(App, {
+    propsData: {
+      fieldLengthX: 10,
+      fieldLengthY: 10
+    }
+  });
 });
 
 afterEach(() => {
   wrapper = null;
 });
 
-describe("Rating", () => {
-  it("renders the main div", () => {
+describe("check html elements ", () => {
+  it("check main element exists ", () => {
     const main = wrapper.findAll(".main");
 
     expect(main.length).toBe(1);
+  });
+
+  // player field
+  it("renders the field box", () => {
+    const playerField = wrapper.findAll(".player ");
+
+    expect(playerField.length).toBe(1);
+  });
+  it("renders the rows", () => {
+    const rows = wrapper.findAll(".player .row");
+
+    expect(rows.length).toBe(10);
+  });
+  it("renders the cells", () => {
+    const cells = wrapper.findAll(".player .cell");
+    expect(cells.length).toBe(100);
+  });
+
+  // enemy field
+  it("renders the field box", () => {
+    const enemyField = wrapper.findAll(".enemy ");
+
+    expect(enemyField.length).toBe(1);
+  });
+  it("renders the rows", () => {
+    const rows = wrapper.findAll(".enemy .row");
+
+    expect(rows.length).toBe(10);
+  });
+  it("renders the cells", () => {
+    const cells = wrapper.findAll(".enemy .cell");
+    // 10 * 10
+    expect(cells.length).toBe(100);
   });
 });
