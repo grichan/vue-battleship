575ec1fa8cb381e0669edd9d4d983d9741507b9a
Vlad <vladgrichan@gmail.com>
2021-02-08 15:31:06 +0000

    field on click adding ship

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <div class="player field">
       <div class="row" v-for="(row, y) in playerField" :key="y">
         <div
-          @click="placeShip($event)"
+          @click="shipSelected($event)"
           class="cell"
           v-for="(col, x) in row"
           :key="col"
@@ -18,8 +18,14 @@
     <div>
       <button>Start new game</button>
       <p>Your turn</p>
-      <div>Ships to place remaining:</div>
-      <div v-for="ship in playerShips" :key="ship">
+      <div>Click on ship to place:</div>
+      <div
+        @click="shipSelected($event)"
+        class="ship"
+        v-for="ship in playerShips"
+        :key="ship"
+        :data-ship="ship[0] + '-' + ship[1]"
+      >
         {{ ship[0] }}x{{ ship[1] }}
         <span v-for="i in ship[0]" :key="i">
           🚢
@@ -43,12 +49,54 @@ export default {
   methods: {
     placeShip(e) {
       e.preventDefault();
-      console.log("e :>> ", e.target.dataset);
+      console.log("e :>> ", e.target.dataset.position);
       console.log("object :>> ");
+    },
+    shipSelected(e) {
+      e.preventDefault();
+      console.log("e :>> ", e.target.dataset.position);
+      console.log("object :>> ");
+      let position = e.target.dataset.position.split("-");
+      console.log("position :>> ", position);
+      let y = position[0];
+      let x = position[1];
+      // 0 = free
+      // 1 = taken
+      // if(this.adding) {
+
+      // }
+
+      if (this.playerField[y][x] === 0 && this.adding == false) {
+        this.playerField[y][x] = 1;
+        this.shipAdding.push([y, x]);
+        this.adding = true;
+      }
+
+      if (this.adding == true && this.playerField[y][x] != 1) {
+        this.shipAdding.forEach((arr) => {
+          if (arr[0] != y && arr[1] != x) {
+            if (
+              y < arr[0] + 3 &&
+              y > arr[0] - 3 &&
+              x < arr[0] + 3 &&
+              x > arr[0] - 3
+            ) {
+              this.playerField[y][x] = 1;
+              this.shipAdding.push([y, x]);
+            }
+          }
+        });
+      }
+      console.log(
+        "this.playerField[position[0],position[1]] :>> ",
+        this.playerField[y][x]
+      );
     }
   },
   data() {
     return {
+      adding: false,
+      shipAdding: [],
       playerField: [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
